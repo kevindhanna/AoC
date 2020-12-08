227fb50befdeecee7c4cf31e40a33c38915a9baf
Kevin Hanna <kevindhanna@live.com>
2020-12-08 23:32:58 +0000

    maybe don't brute force it

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::time::Instant;
 
 #[derive(Copy, Clone)]
 struct Instruction {
@@ -40,18 +41,18 @@ fn build_instruction_set(instructions: Vec<Vec<&str>>) -> Vec<Instruction> {
     }).collect::<Vec<Instruction>>()
 }
 
-fn clone_set_replace(set: &Vec<Instruction>, index: usize, action: Action) -> Vec<Instruction> {
+fn clone_set_replace(set: &Vec<Instruction>, index: i32, action: Action) -> Vec<Instruction> {
     let mut new_set = set.clone();
-    new_set[index].action = action;
+    new_set[index as usize].action = action;
     new_set
 }
 
-fn brutus(instruction_set: &Vec<Instruction>) -> Result<i32, i32> {
+fn brutus(instruction_set: &Vec<Instruction>) -> Result<i32, Vec<i32>> {
     let mut pointer = Pointer{ head: 0, accumulator: 0 };
     let mut visited: Vec<i32> = Vec::new();
     loop {
         if visited.contains(&pointer.head) {
-            return Err(pointer.accumulator);
+            return Err(visited);
         }
         let instruction = instruction_set[pointer.head as usize];
         visited.push(pointer.head);
@@ -75,24 +76,41 @@ fn brutus(instruction_set: &Vec<Instruction>) -> Result<i32, i32> {
 }
 
 fn part_1(instruction_set: &Vec<Instruction>) -> i32 {
-    if let Err(acc) = brutus(instruction_set) {
-        return acc
+    let mut pointer = Pointer{ head: 0, accumulator: 0 };
+    let mut visited: Vec<i32> = Vec::new();
+    loop {
+        if visited.contains(&pointer.head) {
+            return pointer.accumulator
+        }
+        let instruction = instruction_set[pointer.head as usize];
+        visited.push(pointer.head);
+        match instruction.action {
+            Action::Acc => {
+                pointer.accumulator += instruction.count;
+                pointer.head += 1
+            },
+            Action::Jmp => {
+                pointer.head += instruction.count
+            },
+            Action::Nop => {
+                pointer.head += 1;
+            },
+        }
     }
-    panic!("Shouldn't get here");
 }
 
-fn part_2(instruction_set: &Vec<Instruction>) -> i32 {
+fn part_2_brute(instruction_set: &Vec<Instruction>) -> i32 {
     for (i, instruction) in instruction_set.iter().enumerate() {
         match instruction.action {
             Action::Nop => {
-                let new_set = clone_set_replace(instruction_set, i, Action::Jmp);
+                let new_set = clone_set_replace(instruction_set, i as i32, Action::Jmp);
                 match brutus(&new_set) {
                     Ok(val) => return val,
                     Err(_) => continue
                 }
             },
             Action::Jmp => {
-                let new_set = clone_set_replace(instruction_set, i, Action::Nop);
+                let new_set = clone_set_replace(instruction_set, i as i32, Action::Nop);
                 match brutus(&new_set) {
                     Ok(val) => return val,
                     Err(_) => continue
@@ -102,6 +120,32 @@ fn part_2(instruction_set: &Vec<Instruction>) -> i32 {
         }
     }
     panic!("shouldn't get here");
+}
+
+fn part_2_not_quite_brute(instruction_set: &Vec<Instruction>) -> i32 {
+    if let Err(mut visited) = brutus(instruction_set) {
+        while let Some(index) = visited.pop() {
+            let instruction = instruction_set[index as usize];
+            match instruction.action {
+                Action::Nop => {
+                    let new_set = clone_set_replace(instruction_set, index, Action::Jmp);
+                    match brutus(&new_set) {
+                        Ok(val) => return val,
+                        Err(_) => continue
+                    }
+                },
+                Action::Jmp => {
+                    let new_set = clone_set_replace(instruction_set, index, Action::Nop);
+                    match brutus(&new_set) {
+                        Ok(val) => return val,
+                        Err(_) => continue
+                    }
+                },
+                Action::Acc => continue
+            }
+        }
+    };
+    panic!()
 }
 
 fn main() {
@@ -117,6 +161,13 @@ fn main() {
     let part_1_result = part_1(&instruction_set);
     println!("part 1: {}", part_1_result);
 
-    let part_2_result = part_2(&instruction_set);
+    let mut now = Instant::now();
+    let mut part_2_result = part_2_brute(&instruction_set);
+    println!("brute time: {:?}", Instant::now().duration_since(now));
+
+    now = Instant::now();
+    part_2_result = part_2_not_quite_brute(&instruction_set);
+    println!("maybe not brute time: {:?}", Instant::now().duration_since(now));
+
     println!("part 2: {}", part_2_result);
 }
